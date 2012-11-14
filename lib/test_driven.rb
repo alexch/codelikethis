64737201cf56d170fc7fb9ac7aeffddc6c8e342b
Alex Chaffee <alex@stinky.com>
2012-11-13 18:23:33 -0800

    videos for test-driven

--- lib/test_driven.rb
+++ lib/test_driven.rb
@@ -2,6 +2,9 @@ require "course"
 
 class Course
   TestDriven = Course.new("test_driven") do
-    lesson "test_driven"
+    lesson "test_driven" do
+      video "tFwAailXjeA"
+      video "mzcBfUu15RU"
+    end
   end
 end
