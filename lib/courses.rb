0825d086b8b5ea8fd550e89e6527ecb35015e8a4
Alex Chaffee <alex@stinky.com>
2017-12-19 15:54:44 -0500

    try to fix weird heroku/rackup init error

--- lib/courses.rb
+++ lib/courses.rb
@@ -1,17 +1,18 @@
 require_relative('util')
+require_relative('course')
 require_all('courses')
 
 class Courses
   ALL = [
-      Course::LearnToCode,
-      Course::LearnToCodeWithJavascript,
-      Course::AgileDevelopment,
-      Course::Ruby,
-      Course::RubyTools,
-      Course::RubyBasics,
-      Course::RubyBlocks,
-      Course::RubyObjects,
-      Course::RubyAdvanced,
-      Course::Javascript,
+      ::Course::LearnToCode,
+      ::Course::LearnToCodeWithJavascript,
+      ::Course::AgileDevelopment,
+      ::Course::Ruby,
+      ::Course::RubyTools,
+      ::Course::RubyBasics,
+      ::Course::RubyBlocks,
+      ::Course::RubyObjects,
+      ::Course::RubyAdvanced,
+      ::Course::Javascript,
   ]
 end
