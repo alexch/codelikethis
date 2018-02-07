69579821238b533572b34fe05a811abf91281ca5
Alex Chaffee <alex@stinky.com>
2018-02-07 18:55:50 -0500

    better twitter embed

--- lib/views.rb
+++ lib/views.rb
@@ -19,8 +19,8 @@ module Views
       # https://twitter.com/settings/widgets
       # https://dev.twitter.com/docs/embedded-timelines
       rawtext <<-HTML
-<a class="twitter-timeline" width="340" height="500" href="https://twitter.com/codelikethis" data-widget-id="294871116827197443">Tweets by @codelikethis</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<a class="twitter-timeline" href="https://twitter.com/codelikethis">Tweets by codelikethis</a> 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       HTML
     end
   end
