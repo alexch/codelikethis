8ea138eec6cdbdfb7ad9e7c12d5436a78faccf4d
Alex Chaffee <alex@stinky.com>
2018-03-15 16:21:25 -0400

    fix merge error

--- public/lessons/www.rb
+++ public/lessons/www.rb
@@ -41,9 +41,9 @@ class Track
     lesson name: "css_layout",
            display_name: "Page Layout With CSS" do
       link href: "https://www.w3schools.com/css/exercise.asp?filename=exercise_positioning1",
-            name: "W3Schools Positioning Exercise"
+           name: "W3Schools Positioning Exercise"
     end
-    
+
     lesson name: "dom_debugging",
            display_name: "DOM Debugging"
 
@@ -65,20 +65,6 @@ class Track
 
     lesson name: 'css_flexbox',
            display_name: "Page Layout with CSS Flexbox" do
-
-    lesson name: "css_frameworks" do
-      link href: "https://blog.webkid.io/css-frameworks-bootstrap-alternatives/"
-    end
-
-    lesson name: "responsive_layout"
-    lab name: 'FreeCodeCamp "Responsive Web Design"'
-
-    lesson name: "bootstrap_intro"
-
-    lesson name: "bootstrap_layout",
-           display_name: "Page Layout with Bootstrap"
-    lab name: 'FreeCodeCamp "Front End Libraries - Bootstrap"'
-
       link href: "https://medium.freecodecamp.org/learn-css-flexbox-in-5-minutes-b941f0affc34",
            name: "Learn CSS Flexbox in 5 Minutes"
       link href: "https://css-tricks.com/snippets/css/a-guide-to-flexbox"
@@ -87,8 +73,24 @@ class Track
            description: "a step-by-step test-driven game that teaches you flexbox"
     end
 
+    lesson name: "css_frameworks" do
+      link href: "https://blog.webkid.io/css-frameworks-bootstrap-alternatives/"
+    end
+
+    lesson name: "responsive_layout"
+    lab name: 'FreeCodeCamp "Responsive Web Design"'
+
+    lesson name: "bootstrap_intro" do
+      link href: "https://blog.webkid.io/css-frameworks-bootstrap-alternatives/"
+    end
+
+    lesson name: "bootstrap_layout",
+           display_name: "Page Layout with Bootstrap"
+    lab name: 'FreeCodeCamp "Front End Libraries - Bootstrap"'
+
+
     lesson name: "page_layout_with_css_grid",
-      display_name: "Page Layout with CSS Grid" do
+           display_name: "Page Layout with CSS Grid" do
       video youtube_id: '7kVeCqQCxlk'
       link href: "https://cssgridgarden.com",
            name: "Grid Garden",
@@ -97,18 +99,13 @@ class Track
 
     lesson name: "responsive_layouts",
            display_name: "Web Design and Responsive Development"
-   lab name: 'FreeCodeCamp "Responsive Web Design - Applied Visual Design"'
-
-    lesson name: "css_frameworks",
-           display_name: "Developing With Bootstrap" do
-      link href: "https://blog.webkid.io/css-frameworks-bootstrap-alternatives/"
-    end
-    lab name: 'FreeCodeCamp "Front End Libraries - Bootstrap"'
-
+    lab name: 'FreeCodeCamp "Responsive Web Design - Applied Visual Design"'
 
     lesson name: "navigation", description: "Nav bars, site maps, anchors, Bootstrap's responsive nav bar... [move to UX track?]"
 
     lesson name: "bootstrap_components", description: "buttons and accordions and such [move to UX track?]"
+
+    project name: "pretty_profile", description: "now that you know how to use styles and layouts, gussy up your profile page"
 
     lesson name: "embedding",
            description: 'how to use Web APIs and embedded widgets to serve maps, videos, surveys, and other content from around the Web inside your own sites' do
