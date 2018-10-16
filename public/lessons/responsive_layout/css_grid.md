cc29ef52cd5a89be36af0a44332bfc912b18cb8c
Joshua Burke <burke.joshua.james@gmail.com>
2018-10-16 09:25:56 -0400

    CSS Grid Intro, more to come

---
+++ public/lessons/responsive_layout/css_grid.md
@@ -0,0 +1,46 @@
+# CSS Grid
+
+- CSS Grid is an advanced 2-Dimensional layout system
+- Can layout entire websites or small components of websites
+- Describes fixed and flexible track areas
+- Allows for precise item placement within areas
+- Provides for alignment of items
+- Supports separate and overlapping items
+
+# Grid Layout Codepen
+
+<p data-height="450" data-theme-id="light" data-slug-hash="zmpdqb" data-default-tab="css,result" data-user="Dangeranger" data-pen-title="CSS Grid Layout - Template Areas" class="codepen">See the Pen <a href="https://codepen.io/Dangeranger/pen/zmpdqb/">CSS Grid Layout - Template Areas</a> by Joshua Burke (<a href="https://codepen.io/Dangeranger">@Dangeranger</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+
+# Basic Grid Example
+
+```html
+@@@html
+
+<div class="wrapper">
+  <div>One</div>
+  <div>Two</div>
+  <div>Three</div>
+  <div>Four</div>
+  <div>Five</div>
+</div>
+```
+
+```css
+@@@css
+.wrapper {
+  display: grid;
+  grid-template-columns: 200px 200px 200px;
+}
+```
+
+# Basic Grid Codepen
+
+<p data-height="407" data-theme-id="light" data-slug-hash="ZqvJbM" data-default-tab="css,result" data-user="Dangeranger" data-pen-title="Basic Grid Example" class="codepen">See the Pen <a href="https://codepen.io/Dangeranger/pen/ZqvJbM/">Basic Grid Example</a> by Joshua Burke (<a href="https://codepen.io/Dangeranger">@Dangeranger</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+
+# Mozilla Grid Tutorial
+
+* Complete the following walkthough to create your first CSS Grid site
+
+[Mozilla CSS Grid Tutorial](https://mozilladevelopers.github.io/playground/css-grid)
--- public/lessons/responsive_layout/responsive_layout.rb
+++ public/lessons/responsive_layout/responsive_layout.rb
@@ -9,15 +9,25 @@ class Track
     # description: "When Twitter released its Bootstrap framework, the Web got a makeover. Suddenly developers had the ability to make web sites with a clean, standard, modern look and feel, with rules for grid layout much clearer than 'divs and floats'. More recently, the CSS Grid standard has been released, and it provides even more power and clarity than Bootstrap."
   ) do
 
-    lesson name: "response_intro"
-
     lesson name: "css_layout",
            display_name: "Page Layout With CSS" do
             topic name: "positioning"
-            
+
       link href: "https://www.w3schools.com/css/exercise.asp?filename=exercise_positioning1",
            name: "W3Schools Positioning Exercise"
     end
+
+    lesson name: "responsive_intro",
+           display_name: "Introduction to Responsive Development" do
+      topic name: "responsive-layout"
+      topic name: "media-queries"
+      link href: "https://www.w3schools.com/html/html_responsive.asp",
+           name: "W3Schools Responsive HTML Development"
+      project name: 'mint_login',
+              href: "https://www.theodinproject.com/courses/html5-and-css3/lessons/html-forms",
+              description: "this is actually a fairly advanced lab that asks you to create and style a login form like Mint.com's. It should be moved to a project, and we should have a lesson on basic FORM element creation and use."
+    end
+
 
     lesson name: "flexbox_layout",
            display_name: "Page Layout With CSS Flexbox" do
@@ -58,7 +68,7 @@ class Track
 
     lesson name: "css_grid",
            display_name: "Page Layout with CSS Grid" do
-      video youtube_id: '7kVeCqQCxlk'
+      video youtube_id: 'FEnRpy9Xfes'
       link href: "https://mozilladevelopers.github.io/playground/css-grid/",
            name: "Mozilla CSS Grid Tutorial",
            description: "A thourough walk-through of CSS Grid and related tools"
