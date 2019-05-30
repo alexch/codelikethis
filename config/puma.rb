808b0f2042dd8dafc5641bc5c1eb8745e2800806
Joshua Burke <burke.joshua.james@gmail.com>
2019-05-30 19:41:02 -0400

    Configure Puma using ENV variables from Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,16 @@
 require 'barnes'
 
-workers 4
-threads 8, 8
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
+min_threads = Integer(ENV['MIN_THREADS'] || 1)
+max_threads = Integer(ENV['MAX_THREADS'] || 4)
+threads min_threads, max_threads
+
 preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 before_fork do
   Barnes.start
