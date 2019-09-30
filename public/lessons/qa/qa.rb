c98b028c859169d1496454375e48faf9721d92b2
Habenzy <robertrstauss@gmail.com>
2019-09-30 13:12:29 -0400

    empty tracks go away

--- public/lessons/qa/qa.rb
+++
@@ -1,9 +0,0 @@
-require "track"
-
-class Track
-  Qa = Track.new(
-    name: "qa",
-    display_name: "QA (Quality Assurance)",
-    description: "Software Quality Assurance comprises many roles and practices, including testing, code review, release management, design, and usability research. We currently have no lessons specifically about QA, but we do invite QA engineers and experts to speak in our classrooms during our bootcamp course."
-  )
-end
